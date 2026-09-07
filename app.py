9bdc8ddc8af146caa02a6e31c2fe26214b7378bb
Ivan Robledo <ivannrobledo64@gmail.com>
2026-09-07 12:53:05 -0300

    Implementar creación de productos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import engine, base
+from database import engine, base, SessionLocal
 from models import Producto, ProductoCreate
 app=FastAPI()
 
@@ -12,4 +12,16 @@ def inicio():
 
 @app.post("/productos")
 def crear_producto(producto: ProductoCreate):
-    return producto 
+    db = SessionLocal()
+
+    nuevo_producto = Producto(
+        nombre=producto.nombre,
+        precio=producto.precio
+    )
+
+    db.add(nuevo_producto)
+    db.commit()
+    db.refresh(nuevo_producto)
+    db.close()
+
+    return nuevo_producto
