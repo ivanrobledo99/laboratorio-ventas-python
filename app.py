849242e407bf535ba491ea4169f7af0454ea72a6
Ivan Robledo <ivannrobledo64@gmail.com>
2026-09-12 19:13:15 -0300

    finalizacion ABM ventas

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from database import engine, base, SessionLocal
-from models import Producto, ProductoCreate
+from models import Producto, ProductoCreate, Venta, VentaCreate, VentaResponse, ProductoResponse
 app=FastAPI()
 
 base.metadata.create_all(bind=engine)
@@ -41,6 +41,7 @@ def Obtener_Producto_id(id: int):
 
     producto=db.query(Producto).filter(Producto.id==id).first()
     if producto is None:
+        db.close()
         raise HTTPException(status_code=404, detail="PRODUCTO NO ENCONTRADO")
 
     db.close()
@@ -53,6 +54,7 @@ def Agregar_Producto_ID(id:int, producto:ProductoCreate):
     producto_db=db.query(Producto).filter(Producto.id==id).first()
 
     if producto_db is None:
+        db.close()
         raise HTTPException(status_code=404, detail="PRODUCTO NO ENCONTRAOD")
 
     producto_db.nombre=producto.nombre
@@ -64,19 +66,123 @@ def Agregar_Producto_ID(id:int, producto:ProductoCreate):
     return producto_db
 
 
-@app.delete("/producto/{id}")
+@app.delete("/productos/{id}",response_model=ProductoResponse)
 def Eliminar_Producto(id:int):
     db=SessionLocal()
 
     producto_db=db.query(Producto).filter(Producto.id==id).first()
 
     if producto_db is None:
+        db.close()
         raise HTTPException(status_code=404, detail="PRODUCTO NO ENCONTRADO")
 
     db.delete(producto_db)
     db.commit()
 
     db.close()
-    return
+    return producto_db
+
+
+@app.post("/ventas",response_model=VentaResponse)
+def Crear_Venta(ventas:VentaCreate):
+    db=SessionLocal()
+
+    producto_db=db.query(Producto).filter(Producto.id == ventas.id_producto).first()
+
+    if producto_db is None:
+            db.close()
+            raise HTTPException(status_code=404, detail="PRODUCTO NO ENCONTRADO")
+    
+
+    Nueva_venta=Venta(
+        fecha=ventas.fecha,
+        hora=ventas.hora,
+        id_producto=ventas.id_producto,
+        cantidad=ventas.cantidad,
+        precio_total=producto_db.precio*ventas.cantidad
+    )
 
     
+    db.add(Nueva_venta)
+    db.commit()
+    db.refresh(Nueva_venta)
+
+    Nueva_venta.producto
+
+    db.close()
+
+    return Nueva_venta
+
+
+@app.get("/ventas")
+def Obtener_ventas():
+    db=SessionLocal()
+
+    Ventas_db=db.query(Venta).all()
+    db.close()
+
+    return Ventas_db
+
+
+@app.get("/ventas/{id}",response_model=VentaResponse)
+def Obtener_venta_id(id:int):
+    db=SessionLocal()
+
+    Venta_db_id=db.query(Venta).filter(Venta.id == id).first()
+
+    if Venta_db_id is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="VENTA NO ENCONTRADA")
+
+    Venta_db_id.producto
+
+    db.close()
+
+    return Venta_db_id
+
+@app.delete("/ventas/{id}")
+def Eliminar_venta(id:int):
+    db=SessionLocal()
+
+    Venta_eliminar=db.query(Venta).filter(Venta.id == id).first()
+
+    if Venta_eliminar is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="Venta no encontrada")
+
+
+    db.delete(Venta_eliminar)
+    db.commit()
+    db.close()
+
+    return
+
+@app.put("/ventas/{id}",response_model=VentaResponse)
+def Agregar_Venta(id:int,venta:VentaCreate):
+    db=SessionLocal()
+
+    Nueva_venta=db.query(Venta).filter(Venta.id == id).first()
+
+    if Nueva_venta is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="VENTA NO ENCONTRADA")
+
+    producto_db=db.query(Producto).filter(Producto.id == venta.id_producto).first()
+
+    if producto_db is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="PRODUCTO NO ENCONTRADO")
+    
+    Nueva_venta.fecha=venta.fecha
+    Nueva_venta.hora=venta.hora
+    Nueva_venta.id_producto=venta.id_producto
+    Nueva_venta.cantidad=venta.cantidad
+    Nueva_venta.precio_total=venta.cantidad*producto_db.precio
+
+    db.commit()
+    db.refresh(Nueva_venta)
+
+    Nueva_venta.producto
+    db.close()
+    
+    return Nueva_venta
--- models.py
+++ models.py
@@ -1,14 +1,54 @@
 from database import base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float,ForeignKey,Date,Time
+from sqlalchemy.orm import relationship
 from pydantic import BaseModel
+from datetime import date,time
 
 class Producto(base):
     __tablename__ = "productos"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String)
     precio = Column(Float)
+    ventas=relationship("Venta", back_populates="producto")
 
 
 class ProductoCreate(BaseModel):
     nombre: str
     precio:float
+
+
+class Venta(base):
+    __tablename__="ventas"
+    id=Column(Integer,primary_key=True,index=True)
+    fecha=Column(Date)
+    hora=Column(Time)
+    id_producto=Column(Integer,ForeignKey("productos.id"))
+    cantidad=Column(Integer)
+    precio_total=Column(Float)
+    producto=relationship("Producto", back_populates="ventas")
+
+
+class VentaCreate(BaseModel):
+    fecha:date
+    hora:time
+    id_producto:int
+    cantidad:int
+
+class ProductoResponse(BaseModel):
+    id:int
+    nombre:str
+    precio:float
+
+    class Config:
+        from_attributes=True
+
+class VentaResponse(BaseModel):
+    id:int
+    fecha:date
+    hora:time
+    producto:ProductoResponse
+    cantidad:int
+    precio_total:float
+
+    class Config:
+        from_attributes=True
